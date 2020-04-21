df0ec2767626634d427a7440de33b7e2275bf20b
01010100b <caveman1917@hotmail.com>
2020-04-21 08:05:47 +0200

    rename them back

--- AuroraLoader/Versions.cs
+++ AuroraLoader/Versions.cs
@@ -28,6 +28,40 @@ namespace AuroraLoader
             return Compare(a, b) == 1;
         }
 
+        public static int Compare2(string a, string b)
+        {
+            var pieces_a = a.Split('.');
+            var pieces_b = b.Split('.');
+
+            for (int i = 0; i < Math.Max(pieces_a.Length, pieces_b.Length); i++)
+            {
+                if (i >= pieces_a.Length)
+                {
+                    return -1;
+                }
+                else if (i >= pieces_b.Length)
+                {
+                    return 1;
+                }
+                else
+                {
+                    var v_a = int.Parse(pieces_a[i]);
+                    var v_b = int.Parse(pieces_b[i]);
+
+                    if (v_a < v_b)
+                    {
+                        return -1;
+                    }
+                    else if (v_a > v_b)
+                    {
+                        return 1;
+                    }
+                }
+            }
+
+            return 0;
+        }
+
         public static int Compare(string a, string b)
         {
             var pieces = a.Split('.');
